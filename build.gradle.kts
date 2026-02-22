c95403483846b67456c00e495c22abee9161f34f
Midnwave <cameron.crenshaw.cameron@gmail.com>
2026-02-22 12:04:59 -0600

    feature(gradle): gradle 8.14 wrapper and build configuration

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    java
+}
+
+group = "com.blockforge"
+version = "1.0.0"
+description = "The most advanced free chest shop plugin"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://jitpack.io")
+    maven("https://maven.enginehub.org/repo/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7.1") {
+        exclude(group = "org.bukkit")
+    }
+
+    // transitive deps excluded to avoid strict version conflicts with paper api
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.12") { isTransitive = false }
+    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.3.9") { isTransitive = false }
+    compileOnly("com.sk89q.worldguard:worldguard-core:7.0.12") { isTransitive = false }
+    compileOnly("com.sk89q.worldedit:worldedit-core:7.3.9") { isTransitive = false }
+
+    implementation("org.bstats:bstats-bukkit:3.1.0")
+
+    compileOnly("org.xerial:sqlite-jdbc:3.47.2.0")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+tasks.compileJava {
+    options.encoding = "UTF-8"
+    options.release.set(21)
+}
+
+tasks.processResources {
+    filesMatching("plugin.yml") {
+        expand("version" to project.version)
+    }
+}
+
+tasks.jar {
+    archiveBaseName.set("ChestMarketPlus")
+
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }) {
+        include("org/bstats/**")
+    }
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
